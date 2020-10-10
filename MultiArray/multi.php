8a94d3f4f739678bb525596221cc2baea19bae65
halawimaher <halawimaher@gmail.com>
2020-10-10 21:45:52 +0300

    fixed case sensitivity check on string

--- MultiArray/multi.php
+++ MultiArray/multi.php
@@ -4,8 +4,10 @@ $genres = array (
     array("Musicals",'Oklahoma','The Music Man', 'South Pacific'),
     array("Dramas",'Lawrence of Arabia','To Kill a Mockingbird', 'Casablanca'),
     array("Mysteries", 'The Maltese Falcon', 'Rear Window', 'North by Northwest'),
+   
   );
-  echo $genres[0][0]."<br> - - - - >"." 0 = ".$genres[0][1]."<br> - - - - >"." 1 = ".$genres[0][2]."<br> - - - - >"." 2 = ".$genres[0][2]."<br>";
+
+  echo $genres[0][0]."<br> - - - - >"." 0 = ".$genres[0][1]."<br> - - - - >"." 1 = ".$genres[0][2]."<br> - - - - >"." 2 = ".$genres[0][3]."<br>";
   echo $genres[1][0]."<br> - - - - >"." 0 = ".$genres[1][1]."<br> - - - - >"." 1 = ".$genres[1][2]."<br> - - - - >"." 2 = ".$genres[1][3]."<br>";
   echo $genres[2][0]."<br> - - - - >"." 0 = ".$genres[2][1]."<br> - - - - >"." 1 = ".$genres[2][2]."<br> - - - - >"." 2 = ".$genres[2][3]."<br>";
   
